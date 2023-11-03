ea056a9c3652858bf0d6e0d18cc3712721365de5
mchxyz <michellearguelles@protonmail.com>
2023-11-03 13:14:15 -0400

    update-1

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const db = mongoose.connection;
 
 db.on("connected", () => {
-    console.log("connected to MongoDB @" + db.host);
+  console.log("connected to MongoDB @" + db.host);
 });
 
 module.exports = mongoose.connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
